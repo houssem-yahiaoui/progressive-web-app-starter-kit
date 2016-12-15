2c9095df533a97501fd3f4249d2df74be8c00a8f
Houssem Yahiaoui <houssem.yahiaoui.ai@gmail.com>
2016-12-15 18:05:49 +0100

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,11 +4,11 @@ importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
 
 // [*] Firebase Configurations
 var config = {
-    apiKey: "AIzaSyBHQIhiJ4g8QzTOpT5Iq3zDAvtj04xgfmM",
-    authDomain: "fcm-gdg-tlemcen.firebaseapp.com",
-    databaseURL: "https://fcm-gdg-tlemcen.firebaseio.com",
-    storageBucket: "fcm-gdg-tlemcen.appspot.com",
-    messagingSenderId: "565646927628"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 
 //[*] Initializing our Firebase Application.
