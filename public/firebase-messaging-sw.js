d9cabb8fcacafa0f7a5b0475de07c76a32d033ed
Houssem Yahiaoui <houssem.yahiaoui.ai@gmail.com>
2016-12-16 19:10:55 +0100

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,11 +18,11 @@ firebase.initializeApp(config);
 const messaging = firebase.messaging();
 
 // [*] SW Install State Event.
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
   console.log("Install Step, let's cache some files =D");
   //[*] Let's cache a bit !
   event.waitUntil(
-       caches.open('pwa').then(function(cache) {
+       caches.open('pwa').then((cache) => {
             return cache.addAll([
                 '/',
                 '/index.html',
@@ -33,12 +33,12 @@ self.addEventListener('install', function(event) {
 });
 
 // [*] SW Activate State Event.
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate',(event) => {
 	console.log('Activated!', event);
 });
 
 // [*] SW Fetch Event.
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
 	console.log('Let\'s get some Data');
 	console.log(event.request.url);
   event.respondWith(
@@ -49,14 +49,14 @@ self.addEventListener('fetch', function(event) {
 });
 
 // [*] Special object let us handle our Background Push Notifications
-messaging.setBackgroundMessageHandler(function(payload) {
+messaging.setBackgroundMessageHandler((payload) => {
     console.log();
 
     const notificationOptions = {
         body: payload.data.msg,
         icon: "images/icon.jpg"
     }
-    self.addEventListener('notificationclick', function(event) {
+    self.addEventListener('notificationclick', (event) => {
         var messageId = event.notification.data;
 
         event.notification.close();
